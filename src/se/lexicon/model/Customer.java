029ec605fb66e6a7145fb6700f237bc622ad2602
Valter Ekholm <valterekholm@hotmail.com>
2018-03-20 16:45:29 +0100

    La till FoodService klass för matfunktioner

--- src/se/lexicon/model/Customer.java
+++ src/se/lexicon/model/Customer.java
@@ -6,5 +6,67 @@ public class Customer {
 	private String Surname;
 	private Dish dish;
 	
+	public Customer() {
+		
+	}
+	
+	public Customer(String firstName, String surname, Dish dish) {
+		super();
+		this.firstName = firstName;
+		Surname = surname;
+		this.dish = dish;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return Surname;
+	}
+
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		Surname = surname;
+	}
+
+	/**
+	 * @return the dish
+	 */
+	public Dish getDish() {
+		return dish;
+	}
+
+	/**
+	 * @param dish the dish to set
+	 */
+	public void setDish(Dish dish) {
+		this.dish = dish;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	
+	
 	
 }
--- src/se/lexicon/model/FoodService.java
+++ src/se/lexicon/model/FoodService.java
@@ -2,6 +2,7 @@ package se.lexicon.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FoodService {
@@ -10,6 +11,20 @@ public class FoodService {
 
 	public FoodService() {
 		super();
+		Dish dish1 = new Dish(ComfortType.BUSINESS, "Entrecote med svamp", "Stekt av mästerkock", 345);
+		Dish dish2 = new Dish(ComfortType.BUSINESS, "Kolja med rysk kaviar", "Kaviar från svarta marknaden", 359);
+		Dish dish3 = new Dish(ComfortType.BUSINESS, "Finsk rotfruktslåda, eko", "Handplockade rotfrukter", 375);
+		
+		Dish dish_e1 = new Dish(ComfortType.ECONOMY, "Hamburgare med pommes frites", "Simple and fast", 115);
+		Dish dish_e2 = new Dish(ComfortType.ECONOMY, "Fiskgratäng med mos", "En lagom måltid", 110);
+		Dish dish_e3 = new Dish(ComfortType.ECONOMY, "Pannkakor med sylt", "Sylt med låg sockerhalt", 95);
+		
+		dishes.add(dish1);
+		dishes.add(dish2);
+		dishes.add(dish3);
+		dishes.add(dish_e1);
+		dishes.add(dish_e2);
+		dishes.add(dish_e3);		
 	}
 	
 	public void addDish(Dish dish) {
@@ -19,8 +34,17 @@ public class FoodService {
 	public List<Dish> getAllDishes(){
 		return Collections.unmodifiableList(dishes);
 	}
-	
+	/**
+	 * Returns dishes with supplied ComfortType
+	 * @param comfortType
+	 * @return
+	 */
 	public List<Dish> getDishesByComfortType(ComfortType comfortType){
 		return dishes.stream().filter(d -> d.comfortType == comfortType).collect(Collectors.toList());
+	}
+	
+	//Ska kunna söka ut dishes på valfritt vilkor... ej testad
+	public List<Dish> getDishByPredicate(Predicate<? super Dish> predicate){
+		return dishes.stream().filter(predicate).collect(Collectors.toList());
 	}
 }
