9f7ddac7362ce0d2fead58b303ef11e99c40ade1
Valter Ekholm <valterekholm@hotmail.com>
2018-03-21 09:59:35 +0100

    FlightManager assigDish... console-info-text ut

--- src/se/lexicon/model/FlightManager.java
+++ src/se/lexicon/model/FlightManager.java
@@ -37,13 +37,22 @@ public class FlightManager {
 	//Food section
 	public boolean assignDishToCustomer(Customer customer, Dish dish) {
 		for(Seat seat : airplane.getAllSeats()) {//TODO: förenkla sökning
+			System.out.println("Kollar säte " + seat);
 			if(seat.getCustomer().equals(customer)) {
+				System.out.println("Customer " + customer  + " equals " + seat.getCustomer());
 				if(foodService.getDishesByComfortType(seat.getComfortType()).contains(dish)) {
+					System.out.println("Dish " + dish + " återfanns i dishes med CT " + seat.getComfortType());
 					//Om föreslagen maträtt finns Bland alla maträtter som har samma ComfortType som kundens (sätes) ComfortType
 					//Då godkänns tilldelning
 					customer.setDish(dish);
 					return true;
 				}
+				else {
+					System.out.println("Dish återfanns ej");
+				}
+			}
+			else {
+				System.out.println("Customer " + customer + " ej equals " + seat.getCustomer());
 			}
 		}
 		return false;
--- testing/se/lexicon/modelTest/Test_180321.java
+++ testing/se/lexicon/modelTest/Test_180321.java
@@ -13,11 +13,14 @@ import se.lexicon.model.FlightManager;
 public class Test_180321 {
 	
 	FlightManager fm;
+	Customer cust;
 
 	@Before
 	public void setUp() throws Exception {
 		fm = new FlightManager();
-		fm.addCustomer(new Customer(), ComfortType.BUSINESS);//index 0
+		cust = new Customer("Erik");
+		fm.addCustomer(cust, ComfortType.BUSINESS);//index 0
+		
 	}
 	
 
@@ -25,7 +28,7 @@ public class Test_180321 {
 	@Test
 	public void testAssignAvailableDishToCustomer() {
 		Dish dish = fm.getAvailableDishes().get(0);
-		
+		assertTrue(fm.assignDishToCustomer(cust, dish));
 	}
 
 }
