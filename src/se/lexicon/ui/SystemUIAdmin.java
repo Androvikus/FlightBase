3954ee33d161f6afd8a277389c852ae3f3ba3217
magdabq <bergqv.m@gmail.com>
2018-03-23 13:39:13 +0100

    ny systemUIAdmin

--- src/se/lexicon/ui/SystemUIAdmin.java
+++ src/se/lexicon/ui/SystemUIAdmin.java
@@ -1,6 +1,6 @@
 package se.lexicon.ui;
 
-
+//no saved customers in airplane will result in value 0 in both methods
 
 import se.lexicon.model.FlightManager;
 import se.lexicon.model.UIScannerGuard;
@@ -19,34 +19,35 @@ public class SystemUIAdmin {
 	public void start() {
 		
 		System.out.println("");
-		System.out.println("Enter 1 for Total flight income enter 2 for Flight profit");
-		System.out.println(fm.getNumberOfAvailableSeats());
+		System.out.println("Statistics");
+		System.out.println("1. Total flight income \n2. Flight profit \n3. Exit Statistics"); 
+		//System.out.println(fm.getNumberOfAvailableSeats());
 		
-		choice = scannerGuard.readInt("Enter a number: ");
-		
+		choice = scannerGuard.readInt("Enter a number: ");		
 		switch (choice) {
 		
 		case 1:
-			System.out.println("Total income from flight is:");
-			
+			System.out.println("Total income from flight is:  ");			
 			System.out.println(fm.getTotalFlightPrice(fm.getAllSeats()));
-		
-		
+			start();
+			
 		break;
 		
-		
 		case 2:
-			System.out.println("Flight Profit");
+			System.out.println("Flight Profit   ");
 			System.out.println(fm.countProfitPerFlight(fm.getAllSeats()));
+			start();			
+			break;		
 			
 			
-			
+		case 3:
+		
+		System.out.println("Welcome back another time");
+		break;
+		
+		default:
+			System.out.println("Not a valid choice");
 			break;
-			
-			
-			
-		//default
-		//break;
 		}
 		
 	}
