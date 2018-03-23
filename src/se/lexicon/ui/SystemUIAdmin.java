295867d790d336a603547568c24c3cf91c5e2a08
magdabq <bergqv.m@gmail.com>
2018-03-23 12:54:23 +0100

    ändringar iUi admin

--- src/se/lexicon/ui/SystemUIAdmin.java
+++ src/se/lexicon/ui/SystemUIAdmin.java
@@ -2,13 +2,19 @@ package se.lexicon.ui;
 
 import com.sun.java_cup.internal.runtime.Scanner;
 
+import se.lexicon.model.FlightManager;
 import se.lexicon.model.UIScannerGuard;
 //import se.lexicon.model.FlihtManager;
 
+
 public class SystemUIAdmin {
 	
+	private FlightManager fm = new FlightManager();
+	
 	UIScannerGuard scannerGuard = new UIScannerGuard();
-	int choice = 0;
+	private int choice = 0;
+	
+	
 	
 	public void start() {
 		
@@ -17,6 +23,28 @@ public class SystemUIAdmin {
 		
 		choice = scannerGuard.readInt("Enter a number: ");
 		
+		switch (choice) {
+		
+		case 1:
+			System.out.println("Total income from flight is:");
+		System.out.println(fm.getTotalFlightPrice();
+		
+		
+		break;
+		
+		
+		case 2:
+			System.out.println("Flight Profit");
+			
+			
+			
+			break;
+			
+			
+			
+		default
+		break;
+		}
 		
 	}
 	
